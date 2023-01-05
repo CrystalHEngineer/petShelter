0fad4510693c2329e2e99f155ffa3f4e453ef15b
Crystal Harper <crystalhaylen@gmail.com>
2023-01-04 20:54:59 -0600

    Edited server to remove body-parser

--- server/server.js
+++ server/server.js
@@ -4,10 +4,6 @@ const cors = require('cors');
 const port = 8000;
 require("dotenv").config()
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
-const bodyParser = require("body-parser")
-
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
