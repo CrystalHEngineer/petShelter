a07c3a4c3e5f3ec3a5d9dd0e54ebef46f2333092
Crystal Harper <crystalhaylen@gmail.com>
2023-01-10 17:36:04 -0600

    Edited server.js and package.json some more....

--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ const port = 8000;
 require("dotenv").config()
 // const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 app.use(express.json());
+app.get("'", (req, res) => {
+  res.sendFile(path.join(dirname, "..", "client", "build", "index.html"));
+});
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use((req, res, next) => {
     res.header({"Access-Control-Allow-Origin": "*"});
     next();
-  })  
+  });
+
 
 
 require('./config/mongoose.config');
