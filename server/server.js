1af06b52cc4df78a291be32cc8d382faa2a83bce
Crystal Harper <crystalhaylen@gmail.com>
2023-01-12 13:46:25 -0600

    Edited a link

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
     res.header({"Access-Control-Allow-Origin": "*"});
     next();
   });
-app.get("'", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
