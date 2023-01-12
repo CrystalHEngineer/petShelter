cb3680e6f444f50a4ecc9054aec7d30b1190ebb4
Crystal Harper <crystalhaylen@gmail.com>
2023-01-12 14:03:43 -0600

    Edited the server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
-
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 require('./config/mongoose.config');
 
